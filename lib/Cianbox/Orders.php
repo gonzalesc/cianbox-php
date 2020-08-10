b7e1f728e58b51548e1dd2c6d11e6aedff26eda6
gonzalesc <alex@gonzalesc.org>
2020-08-10 08:07:27 -0300

    fix: path to GET and POST

--- lib/Cianbox/Orders.php
+++ lib/Cianbox/Orders.php
@@ -9,15 +9,26 @@ namespace Cianbox;
  */
 class Orders extends Resource {
 
-	const URL_ORDERS = "/pedidos/alta";
+	const URL_ORDERS_GET = "/pedidos/lista";
+	const URL_ORDERS_POST = "/pedidos/alta";
+	
 
 	/**
      * @param array|null $options
      *
      * @return create Customer response.
      */
-    public function create($options = NULL) {
-        return $this->request("POST", self::URL_ORDERS, $this->cianbox->Auth->GetToken(), $options);
+    public function get($options = NULL) {
+        return $this->request("GET", self::URL_ORDERS_GET, $this->cianbox->Auth->GetToken(), $options);
     }
 
+
+    /**
+     * @param array|null $options
+     *
+     * @return create Customer response.
+     */
+    public function create($options = NULL) {
+        return $this->request("POST", self::URL_ORDERS_POST, $this->cianbox->Auth->GetToken(), $options);
+    }
 }
