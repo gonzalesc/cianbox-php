e5d1b90865f66832fe485d7c6c13ce548b88633b
Alex <alex@gonzalesc.org>
2022-08-02 23:14:24 -0300

    feature: delete order

--- lib/Cianbox/Orders.php
+++ lib/Cianbox/Orders.php
@@ -11,12 +11,13 @@ class Orders extends Resource {
 
 	const URL_ORDERS_GET = "/pedidos/lista";
 	const URL_ORDERS_POST = "/pedidos/alta";
+    const URL_ORDERS_DELETE = "/pedidos/eliminar";
 	
 
 	/**
      * @param array|null $options
      *
-     * @return create Customer response.
+     * @return get Customer response.
      */
     public function get($options = NULL) {
         return $this->request("GET", self::URL_ORDERS_GET, $this->cianbox->Auth->GetToken(), $options);
@@ -30,5 +31,15 @@ class Orders extends Resource {
      */
     public function create($options = NULL) {
         return $this->request("POST", self::URL_ORDERS_POST, $this->cianbox->Auth->GetToken(), $options);
+    }
+
+
+    /**
+     * @param array|null $options
+     *
+     * @return delete orders
+     */
+    public function delete($options = NULL) {
+        return $this->request("DELETE", self::URL_ORDERS_DELETE, $this->cianbox->Auth->GetToken(), $options);
     }
 }
